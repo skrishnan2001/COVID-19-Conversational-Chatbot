9128dadc30e7d2e9c8290e6072b4771a17b878f5
srikrishna98 <srikrishna0412@gmail.com>
2020-05-13 00:23:08 +0530

    fixed start_services add path issue. Docker build successful

---
+++ new_rasa_project/app/start_services.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Start rasa server with nlu model
+rasa run --model models/20200510-230829.tar.gz --enable-api --cors "*" --debug -p 5005
--- new_rasa_project/start_services.sh
+++
@@ -1,2 +0,0 @@
-# Start rasa server with nlu model
-rasa run --model models/20200510-230829.tar.gz --enable-api --cors "*" --debug -p $PORT
