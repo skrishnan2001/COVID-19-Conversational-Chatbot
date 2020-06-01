a37bf064b86898f96acd6036ba568994a02e72af
vishnu-vasan <58689453+vishnu-vasan@users.noreply.github.com>
2020-06-01 19:24:21 +0530

    Add files via upload

--- new_rasa_project/app/start_services.sh
+++ new_rasa_project/app/start_services.sh
@@ -1,2 +1,2 @@
 # Start rasa server with nlu model
-rasa run --model models/20200601-183450.tar.gz --enable-api --cors "*" --debug -p $PORT
+rasa run --model models/20200601-190752.tar.gz --enable-api --cors "*" --debug -p $PORT
