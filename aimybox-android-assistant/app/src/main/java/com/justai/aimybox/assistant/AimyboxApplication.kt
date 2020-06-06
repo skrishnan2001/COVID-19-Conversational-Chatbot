4427258683215c66a212cb4a341dac2ef78fff35
srikrishna98 <srikrishna0412@gmail.com>
2020-06-06 21:09:42 +0530

    android added

---
+++ aimybox-android-assistant/app/src/main/java/com/justai/aimybox/assistant/AimyboxApplication.kt
@@ -0,0 +1,31 @@
+package com.justai.aimybox.assistant
+
+import android.app.Application
+import android.content.Context
+import com.justai.aimybox.Aimybox
+import com.justai.aimybox.dialogapi.rasa.RasaDialogApi
+import com.justai.aimybox.components.AimyboxProvider
+import com.justai.aimybox.core.Config
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
+import java.util.*
+
+class AimyboxApplication : Application(), AimyboxProvider {
+
+    companion object {
+        private const val AIMYBOX_API_KEY = "Ldf0j7WZi3KwNah2aNeXVIACz0lb9qMH"
+    }
+
+    override val aimybox by lazy { createAimybox(this) }
+
+    private fun createAimybox(context: Context): Aimybox {
+        val unitId = UUID.randomUUID().toString()
+
+        val textToSpeech = GooglePlatformTextToSpeech(context, Locale.ENGLISH)
+        val speechToText = GooglePlatformSpeechToText(context, Locale.ENGLISH)
+
+        val dialogApi = RasaDialogApi(unitId, "https://covid--chatbot.herokuapp.com/webhooks/rest/webhook")
+
+        return Aimybox(Config.create(speechToText, textToSpeech, dialogApi))
+    }
+}
