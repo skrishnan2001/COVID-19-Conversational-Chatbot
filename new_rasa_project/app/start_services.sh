64149c861c5cb5c2fbd2a9a2150af436093ad2f2
Srikrishna <srikrishna0412@gmail.com>
2020-06-26 13:21:19 +0530

    Update start_services.sh

--- new_rasa_project/app/start_services.sh
+++ new_rasa_project/app/start_services.sh
@@ -1,2 +1,5 @@
 # Start rasa server with nlu model
-rasa run --model models/20200625-225413.tar.gz --enable-api --cors "*" --debug -p $PORT ; rasa run actions
+rasa run actions &
+
+rasa run --model models/20200625-225413.tar.gz --enable-api --cors "*" --debug \
+        --endpoints endpoints.yml -p $PORT ;
